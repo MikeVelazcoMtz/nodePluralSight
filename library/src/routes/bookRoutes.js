db63e6917fbff1e5cda0325eecc4620a65bf2566
Miguel Angel Velazco martinez <velazcomtz.miguel@gmail.com>
2018-03-31 00:36:20 -0600

    Routing
    Router Functions:
    - Returning and implementing router as a function.

--- library/src/routes/bookRoutes.js
+++ library/src/routes/bookRoutes.js
@@ -2,57 +2,55 @@ var express = require('express');
 
 var bookRouter = express.Router();
 
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-    },
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: false
-    },
-    {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        author: 'Henry Kuttner',
-        read: false
-    },
-    {
-        title: 'A Journey into the Center of the Earth',
-        genre: 'Science Fiction',
-        author: 'Jules Verne',
-        read: false
-    }
-];
-
-bookRouter.route('/').get(function (req, res) {
-    res.render('books', {
-            title: 'Hello from render',
-            nav: [
-                {Link:'/Books', Text: 'Books'},
-                {Link:'/Authors', Text: 'Authors'}
-            ],
-            books: books
+var router = function (nav) {
+    var books = [
+        {
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Misérables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
+        },
+        {
+            title: 'The Dark World',
+            genre: 'Fantasy',
+            author: 'Henry Kuttner',
+            read: false
+        },
+        {
+            title: 'A Journey into the Center of the Earth',
+            genre: 'Science Fiction',
+            author: 'Jules Verne',
+            read: false
         }
-    );
-});
+    ];
 
-bookRouter.route('/:id').get(function (req, res) {
-    var id = req.params.id;
+    bookRouter.route('/').get(function (req, res) {
+        res.render('books', {
+                title: 'Hello from render',
+                books: books,
+                nav: nav
+            }
+        );
+    });
 
-     res.render('book', {
-            title: 'Hello from render',
-            nav: [
-                {Link:'/Books', Text: 'Books'},
-                {Link:'/Authors', Text: 'Authors'}
-            ],
-            book: books[id]
-        }
-    );
-});
+    bookRouter.route('/:id').get(function (req, res) {
+        var id = req.params.id;
 
-module.exports = bookRouter;
+         res.render('book', {
+                title: 'Hello from render',
+                nav: nav,
+                book: books[id]
+            }
+        );
+    });
+
+    return bookRouter;
+}
+
+module.exports = router;
