04ad8025b798bb015f7a8a7052abcce81fe60f52
mvelazcoItexico <mvelazco@itexico.net>
2018-04-03 15:54:01 -0500

    Structure and 3rd Party APIs
    HTTP(S):
      - Making REST requests (goodreads API only worked with HTTPS requests) to get books descriptions.

--- library/src/services/goodreadsService.js
+++ library/src/services/goodreadsService.js
@@ -1,6 +1,30 @@
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
+var key = process.env.GOODREADS_KEY;
+
 var goodreadService = function () {
     var getBookById = function (id, callback) {
-        callback(null, {description: 'Our description'});
+        var options = {
+            host: 'www.goodreads.com',
+            path: '/book/show/50.xml?key=' + key
+        };
+
+        var requestCallBack = function (response) {
+            var str = '';
+
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            response.on('end', function () {
+                parseString(str, function (err, result) {
+                    callback(null, result.GoodreadsResponse.book[0]);
+                });
+            });
+        };
+
+        http.get(options, requestCallBack).end();
     };
 
     return {
